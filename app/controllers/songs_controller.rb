dcbbe9614d74e6488283e7664803c8e615b48a7c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-06 20:44:13 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,7 +6,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    raise params.inspect
+    raise params["file"].inspect
      CSV.foreach(params[:titles].path, headers: true) do |title| 
        Song.create(title: title[0])
      
